ae25e93c969710fdec603e7def1728de59d26e37
Yehia-Gamal <yahia.elspaa@gmail.com>
2022-12-31 20:30:20 +0200

    add skills section and content is and background canvas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,23 @@
-// Header + Navbar position Fixed
-let header = document.querySelector("header");
-let landing = document.querySelector(".landing");
-
-// My Props
-let headerHeight = header.style.height;
-let headerPadding = header.style.padding;
-
-window.onscroll = function () {
-  let windowH = window.scrollY;
-  let headerH = header.offsetHeight; //height + padding + border
-  let landingH = landing.offsetHeight; //height + padding + border
-
-  if (windowH > headerH) {
-    header.style.height = "0";
-    header.style.padding = "0";
-    if (windowH >= landingH) {
-      header.style.height = headerHeight;
-      header.style.padding = headerPadding;
-      header.style.position = "fixed";
-      header.style.boxShadow = "0 8px 32px 0 rgb(0 0 0 / 37%)";
-      header.style.background =
-        "linear-gradient(135deg,rgba(255, 255, 255, 0.1),rgba(255, 255, 255, 0))";
-      header.style.backdropFilter = "blur(5px)";
-      header.style.borderBottom = "1px solid rgba(255, 255, 255, 0.18)";
-      header.style.zIndex = "999";
-    } else {
-      header.style.position = "relative";
-      header.style.boxShadow = "none";
-    }
-  } else {
-    header.style.height = headerHeight;
-    header.style.padding = headerPadding;
-  }
+window.onload = function () {
+  Particles.init({
+    selector: ".background",
+  });
 };
+const particles = Particles.init({
+  selector: ".background",
+  color: ["#03dac6", "#ff0266", "#000000"],
+  connectParticles: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      options: {
+        color: ["#faebd7", "#03dac6", "#ff0266"],
+        maxParticles: 43,
+        connectParticles: false,
+      },
+    },
+  ],
+});
 
 // Auto Text Effect
 const textEl = document.querySelector(".text-auto");
@@ -56,3 +41,111 @@ function writeText() {
 }
 
 speedEl.addEventListener("input", (e) => (speed = 300 / e.target.value));
+
+class CodeTechStation {
+  constructor() {
+    this.currentId = null;
+    this.currentTab = null;
+    this.tabContainerHeight = 70;
+    this.lastScroll = 0;
+    let self = this;
+    $(".nav-tab").click(function () {
+      self.onTabClick(event, $(this));
+    });
+    $(window).scroll(() => {
+      this.onScroll();
+    });
+    $(window).resize(() => {
+      this.onResize();
+    });
+  }
+
+  onTabClick(event, element) {
+    event.preventDefault();
+    let scrollTop =
+      $(element.attr("href")).offset().top - this.tabContainerHeight + 1;
+    $("html, body").animate({ scrollTop: scrollTop }, 600);
+  }
+
+  onScroll() {
+    this.checkHeaderPosition();
+    this.findCurrentTabSelector();
+    this.lastScroll = $(window).scrollTop();
+  }
+
+  onResize() {
+    if (this.currentId) {
+      this.setSliderCss();
+    }
+  }
+
+  checkHeaderPosition() {
+    const headerHeight = 75;
+    if ($(window).scrollTop() > headerHeight) {
+      $(".nav-container").addClass("nav-container--scrolled");
+    } else {
+      $(".nav-container").removeClass("nav-container--scrolled");
+    }
+    let offset =
+      $(".nav").offset().top +
+      $(".nav").height() -
+      this.tabContainerHeight -
+      headerHeight;
+    if (
+      $(window).scrollTop() > this.lastScroll &&
+      $(window).scrollTop() > offset
+    ) {
+      $(".nav-container").addClass("nav-container--move-up");
+      $(".nav-container").removeClass("nav-container--top-first");
+      $(".nav-container").addClass("nav-container--top-second");
+    } else if (
+      $(window).scrollTop() < this.lastScroll &&
+      $(window).scrollTop() > offset
+    ) {
+      $(".nav-container").removeClass("nav-container--move-up");
+      // $(".nav-container").removeClass("nav-container--top-second");
+      $(".nav-container-container").addClass("nav-container--top-first");
+    } else {
+      $(".nav-container").removeClass("nav-container--move-up");
+      $(".nav-container").removeClass("nav-container--top-first");
+      $(".nav-container").removeClass("nav-container--top-second");
+    }
+  }
+
+  findCurrentTabSelector(element) {
+    let newCurrentId;
+    let newCurrentTab;
+    let self = this;
+    $(".nav-link").each(function () {
+      let id = $(this).attr("href");
+      let offsetTop = $(id).offset().top - self.tabContainerHeight;
+      let offsetBottom =
+        $(id).offset().top + $(id).height() - self.tabContainerHeight;
+      if (
+        $(window).scrollTop() > offsetTop &&
+        $(window).scrollTop() < offsetBottom
+      ) {
+        newCurrentId = id;
+        newCurrentTab = $(this);
+      }
+    });
+    if (this.currentId != newCurrentId || this.currentId === null) {
+      this.currentId = newCurrentId;
+      this.currentTab = newCurrentTab;
+      this.setSliderCss();
+    }
+  }
+
+  setSliderCss() {
+    let width = 0;
+    let left = 0;
+    if (this.currentTab) {
+      width = this.currentTab.css("width");
+      left = this.currentTab.offset().left;
+    }
+    $(".nav-tab-slider").css("width", width);
+    $(".nav-tab-slider").css("left", left);
+  }
+}
+
+new CodeTechStation();
