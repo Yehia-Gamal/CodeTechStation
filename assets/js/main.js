1566da7210180b37cf3b372658e90cfda68e9f58
Yehia-Gamal <yahia.elspaa@gmail.com>
2023-01-23 18:34:04 +0200

    Add Scroll Animaition With React JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,6 +78,29 @@ function writeText() {
 
 speedEl.addEventListener("input", (e) => (speed = 300 / e.target.value));
 
+// Scroll Animaition
+
+let boxes = document.querySelectorAll("main .box");
+
+window.addEventListener("scroll", sheckBoxes);
+
+sheckBoxes();
+
+function sheckBoxes() {
+  let triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    let boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
+
+
 class CodeTechStation {
   constructor() {
     this.currentId = null;
